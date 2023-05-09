201d1450f74ce541caa15721037195f24ac0dbf0
sagaraanjna <sagaraanjna59514@gmail.com>
2023-05-09 10:18:15 +0530

    up

---
+++ main.js
@@ -0,0 +1,6 @@
+function multiply()
+{
+    num1=document.getElementById("first").value;
+    num2=document.getElementById("second").value;
+    document.getElementById("result").innerHTML=num1*num2;
+}
